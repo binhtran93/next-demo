2092d1eb8dbaab6bbbbab2215ff77b2fe256d779
Binh.Tran <binh.tranduc@nttdata.com>
2023-05-19 17:21:26 +0700

    in it

---
+++ app/(site)/page.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div
+      className="
+        flex
+        min-h-full
+        flex-col
+        justify-center
+        py-12
+        sm:px-6
+        lg:px-8
+        bg-gray-100
+      "
+    >
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <Image
+          src={"/images/logo.png"}
+          alt="Logo"
+          width={45}
+          height={45}
+          className={"mx-auto w-auto"}
+        />
+      </div>
+
+    </div>
+  )
+}
