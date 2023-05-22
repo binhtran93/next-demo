a0682a82b65637ecede18251621c6556aa305770
binh <binh.tran@smartbox.com>
2023-05-22 07:53:30 +0700

    login

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import ToasterContext from '@/app/context/toast';
+import AuthContext from "@/app/context/AuthContext";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,8 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ToasterContext />
-        {children}
+        <AuthContext>
+            <ToasterContext />
+            {children}
+        </AuthContext>
       </body>
     </html>
   )
